a3a035714986a136621adba102520618d7699041
Juan Moraga <poluxfc@gmail.com>
2022-01-30 20:21:08 -0300

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt -y upgrade
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y zsh wget build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git nsis g++-mingw-w64-x86-64 gcc make perl dkms libssl-dev libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev software-properties-common libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqt4-dev openssl1.0
+sudo apt install -y libssl1.0-dev
