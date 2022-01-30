83f0df4964d4c53914fd010b5fc3dd1efe1dccbf
Juan Moraga <poluxfc@gmail.com>
2022-01-30 20:34:59 -0300

    act

--- init.sh
+++ init.sh
@@ -9,3 +9,14 @@ sudo apt install -y software-properties-common libdb4.8-dev libdb4.8++-dev libmi
 sudo apt install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools
 sudo apt install -y libprotobuf-dev protobuf-compiler libqt4-dev openssl1.0
 sudo apt install -y libssl1.0-dev
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo update-alternatives --config x86_64-w64-mingw32-g++
+git clone https://github.com/litecoin-project/litecoin.git
+cd litecoin
+PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
+cd depends
+make HOST=x86_64-w64-mingw32
+cd ..
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
+make
